0ca565956e837a0b1ef0b96a351815771ffb3973
mlhiter <3076438032@qq.com>
2025-09-16 11:27:46 +0800

    fix: chat list not item -> no create button

--- components/nav-chats.tsx
+++ components/nav-chats.tsx
@@ -6,17 +6,22 @@ import Link from 'next/link'
 import { ChatList } from '@/components/chat/chat-list'
 import { SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenuButton } from '@/components/ui/sidebar'
 
+import { useChats } from '@/hooks/use-chat'
+
 export function NavChats() {
+  const { chats } = useChats()
   return (
     <SidebarGroup className="group-data-[collapsible=icon]:hidden">
       <div className="flex items-center justify-between">
         <SidebarGroupLabel>Chat History</SidebarGroupLabel>
-        <SidebarMenuButton asChild size="sm" className="h-8 w-8">
-          <Link href="/chat">
-            <Plus className="h-3 w-3" />
-            <span className="sr-only">New Chat</span>
-          </Link>
-        </SidebarMenuButton>
+        {chats.length > 0 && (
+          <SidebarMenuButton asChild size="sm" className="h-8 w-8">
+            <Link href="/chat">
+              <Plus className="h-3 w-3" />
+              <span className="sr-only">New Chat</span>
+            </Link>
+          </SidebarMenuButton>
+        )}
       </div>
       <SidebarGroupContent>
         <ChatList />
