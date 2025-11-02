a50a3c9bc77a40b6452c7c162c512e6b2b251ccd
mlhiter <3076438032@qq.com>
2025-11-02 21:01:41 +0800

    feat: sitemap and robot

---
+++ app/sitemap.ts
@@ -0,0 +1,21 @@
+import { type MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
+  const currentDate = new Date()
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: currentDate,
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/preview`,
+      lastModified: currentDate,
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ]
+}
